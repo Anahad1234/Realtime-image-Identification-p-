3bde1fa5ca470baf43636bca1e87b61cca53596e
Anahad1234 <93989180+Anahad1234@users.noreply.github.com>
2021-11-25 18:35:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup()
+{
+    createCanvas(300,300);
+    canvas.center();
+    video.createCaputre(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/qcb2q8-ID/model.json',modelLoaded)
+}
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
+function gotResult()
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
